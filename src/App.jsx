7834c31830e940583569c7ec842f44ddc7137dea
Umar Deshmukh <umardesh@gmail.com>
2026-08-05 15:06:20 +0530

    Fixed the issues and added the navbars

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import React, { useRef } from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Agence from "./pages/Agence";
-import Projects from "./pages/Projects";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import Navbar from "./components/Navigation/navbar";
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
+import Navbar from './components/Navigation/navbar'
+import FullScreenNav from './components/Navigation/FullScreenNav'
+import NavContext from './context/NavContext'
 
-gsap.registerPlugin(useGSAP);
 
 const App = () => {
-
-
-  
-
   return (
-    <div className="text-white bg-gray-500">
-      
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/agence" element={<Agence />} />
-        <Route path="/projects" element={<Projects />} />
-        
-      </Routes>
-    </div>
-  );
-};
+    <NavContext>
+      <div className='overflow-x-hidden'>
+        <Navbar />
+        <FullScreenNav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/agence' element={<Agence />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+      </div>
+    </NavContext>
+  )
+}
 
-export default App;
+export default App
