26135194b28f88773054a283672eca7b37ae1e00
Umar Deshmukh <umardesh@gmail.com>
2026-08-05 14:16:11 +0530

    added NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Agence from "./pages/Agence";
 import Projects from "./pages/Projects";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import Navbar from "./components/Navigation/navbar";
 
 gsap.registerPlugin(useGSAP);
 
@@ -16,11 +17,12 @@ const App = () => {
   return (
     <div className="text-white bg-gray-500">
       
-
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
         <Route path="/projects" element={<Projects />} />
+        
       </Routes>
     </div>
   );
